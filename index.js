d94c5d586680c628e930b72107e8d4d8354a1aa7
Loi Tran <loi@anantadigi.com>
2021-10-19 10:56:11 +0700

    Update

--- index.js
+++ index.js
@@ -13,7 +13,20 @@ var job = new CronJob(
 	},
 	null,
 	true,
-	'America/Los_Angeles'
+	'Asia/Ho_Chi_Minh'
 );
 // Use this if the 4th param is default value(false)
 // job.start()
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
