04831a4d3702258532caff9cd22f73c2ee41f987
Loi Tran <loi@anantadigi.com>
2021-10-20 11:36:50 +0700

    Update

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
 docker build --tag myvpn-node .
-docker images
-
-docker tag myvpn-node:latest nhoxzunbmt/myvpn-node:v1.1
-docker images
-docker push nhoxzunbmt/myvpn-node:v1.1
-
-
+docker tag myvpn-node:latest nhoxzunbmt/myvpn-node:latest
+docker push nhoxzunbmt/myvpn-node:latest
